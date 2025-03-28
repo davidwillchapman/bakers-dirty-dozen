85648849bd10c8cce3b97016f9ad3e21fc8c71c3
David Chapman <davidwillchapman@gmail.com>
2025-03-27 21:34:58 -0700

    home page WIP

---
+++ bdd-client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Paper, Typography } from "@mui/material";
+
+export default function Home() {
+    return (
+        <>
+            <Paper>
+                <Typography>Home Page</Typography>
+            </Paper>
+        </>
+    );
+}
