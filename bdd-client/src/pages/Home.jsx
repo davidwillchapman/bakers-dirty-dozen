043a64a33d4f8834442143c59d6ae363e539977f
David Chapman <davidwillchapman@gmail.com>
2025-03-30 00:19:20 -0700

    homepage content

--- bdd-client/src/pages/Home.jsx
+++ bdd-client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
             <main>
                 <section>
                     <h2>Welcome to the Home of Baker's Dirty Dozen Fantasy Football League!</h2>
-                    <p>Over a decade of fantasy football results to a single place.</p>
+                    <p>Over a decade of fantasy football results coming together in a single place.</p>
                     <p>Stay tuned for more functionality coming soon:
                         <ul>
                             <li>Hall of Fame</li>
